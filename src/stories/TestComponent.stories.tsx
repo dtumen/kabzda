570e1e55724f74d0e3856f8b244e9d0019f9f237
dtumen <dt1109@mail.ru>
2024-06-07 19:33:38 +0200

    add ControlledCheckbox, ControlledInput, ContolledSelect in TestComponent.stories.tsx

--- src/stories/TestComponent.stories.tsx
+++ src/stories/TestComponent.stories.tsx
@@ -1,6 +1,7 @@
 import type {Meta} from '@storybook/react';
 import {TestComponent} from '../components/TestComponent/TestComponent';
 import {ChangeEvent, useRef, useState} from 'react';
+import {action} from '@storybook/addon-actions';
 
 const meta: Meta<typeof TestComponent> = {
     component: TestComponent,
@@ -10,20 +11,6 @@ export default meta;
 
 export const UncontrolledInput = () => <input/>;
 export const ControlledInputWithFixedValue = () => <input value={'Hard!'}/>
-
-export const UncotrolledComponent = () => {
-    const [inputValue, setInputValue] = useState('');
-
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement> ) => setInputValue(e.currentTarget.value);
-
-    return (
-        <>
-            <input value={inputValue} onChange={handleInputChange}/>
-            Значение: {inputValue}
-        </>
-    )
-}
-
 export const GetValueByButtonPress = () => {
     const [inputValue, setInputValue] = useState('');
     const inputRef = useRef<HTMLInputElement>(null);
@@ -40,6 +27,55 @@ export const GetValueByButtonPress = () => {
             <input ref={inputRef}/>
             <button onClick={handleInputChange}>Добавить значение!</button>
             <div>Значение: {inputValue} </div>
+        </>
+    )
+}
+
+export const ControlledInput = () => {
+    const [inputValue, setInputValue] = useState('');
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.currentTarget.value);
+    }
+
+    return (
+        <>
+            <input value={inputValue} onChange={handleInputChange}/>
+            -{inputValue}
+        </>
+    )
+}
+
+export const ControlledCheckbox = () => {
+    const [isChecked, setChecked] = useState(false);
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setChecked(!isChecked);
+    }
+
+    const checkedValue = String(isChecked);
+
+    return (
+        <>
+            <input type={'checkbox'} checked={isChecked} onChange={handleInputChange}/>
+            <p>Value of checkbox: {checkedValue}</p>
+        </>
+    )
+}
+
+export const ControlledSelect = () => {
+    const [selectValue, setSelectValue] = useState<string | undefined>('Moscow');
+    const handleInputChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setSelectValue(e.currentTarget.value);
+    }
+
+    return (
+        <>
+            <select value={selectValue} onChange={handleInputChange}>
+                <option>none</option>
+                <option value={'Moscow'}>Moscow</option>
+                <option value={'Kiev'}>Kiev</option>
+                <option value={'Minsk'}>Minsk</option>
+            </select>
+            Select value: {selectValue}
         </>
     )
 }
