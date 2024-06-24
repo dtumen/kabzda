2adeff894836c66c21709ccc9b2bba0819b550c4
dtumen <dt1109@mail.ru>
2024-06-24 18:58:32 +0200

    21 feat: 21 done

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -1,6 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
-import {useState} from 'react';
+import {useEffect, useState, CSSProperties} from 'react';
 
 const meta: Meta = {
     title: 'React/useEffect',
@@ -12,9 +12,38 @@ export default meta;
 type Story = StoryObj;
 
 export const Counter: Story = {
+    parameters: {
+        layout: 'centered',
+    },
+
     render: () => {
+        const [counter, setCounter] = useState(0);
+        const [title, setTitle] = useState<string | number>('Nothing');
+
+        useEffect(() => {
+            setTitle(counter)
+        }, [counter]);
+
         return (
-            <div>useEffect</div>
+            <div style={styles.container}>
+                <div>
+                    <button style={styles.button} onClick={() => setCounter(prev => prev +1)}>+</button>
+                    { counter }
+                </div>
+                <div>Представим, что мы меняем title страницы</div>
+                Title: { title }
+            </div>
         )
+    }
+};
+
+const styles: { container: CSSProperties, button: CSSProperties  } = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px'
+    },
+    button: {
+        width: '100px',
     }
 }
