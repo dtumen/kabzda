fb77211c5295474c88ac1e7683f160f1776629e8
dtumen <dt1109@mail.ru>
2024-07-05 22:00:59 +0200

    25 feat: update README.md

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -2,6 +2,7 @@ import type {Meta, StoryObj} from '@storybook/react';
 
 import {useEffect, useState, CSSProperties} from 'react';
 
+
 const meta: Meta = {
     title: 'React/useEffect',
     tags: ['autodocs'],
